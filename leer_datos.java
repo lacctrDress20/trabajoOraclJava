cc15899d705a67633b168dd3f2845e57aab50455
lacctrDress20 <1490244@senati.pe>
2023-03-16 11:46:35 -0500

    saludo

---
+++ leer_datos.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/reactjs.jsx to edit this template
+ */
+public class leer_datos{
+    public static void main(String[] args)
+    {  
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("por favor ingrese su nombre");
+        
+        String nombre = sc.nextLine();
+        
+        System.out.println(nombre + "bienvenido");
+        
+    }
+}
