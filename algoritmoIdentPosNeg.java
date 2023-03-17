80a332450dd14a605b0899151cff2c109e2f9c5c
lacctrDress20 <1490244@senati.pe>
2023-03-17 10:58:30 -0500

    determinar el numero es positivo y negativo

--- algoritmoIdentPosNeg.java
+++ algoritmoIdentPosNeg.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
+
+import javax.security.auth.x500.X500Principal;
 public class NegPos{
     public static void main(String[] agrs)
     {
-        Scanner numero = new Scanner(System.in);
+        Scanner numero1 = new Scanner(System.in);
         System.out.println("Ingrese el numero");
+        double numero = numero1.nextDouble();
+        if(numero==0){
+            System.out.println("El numero que ingresaste es neutro");
+        }else if (numero>0){
+            System.out.println("El numero que ingresaste es positivo");
+        }else {
+            System.out.println("El numero que ingresaste es negativo");
+        }
         }
     }
