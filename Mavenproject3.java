5d7b15357b0651bc13a4184ddaf6cd69f12d3f08
lacctrDress20 <1490244@senati.pe>
2023-03-25 10:11:26 -0500

    agregar mas opciones

--- Mavenproject3.java
+++ Mavenproject3.java
@@ -10,10 +10,6 @@ package com.mycompany.mavenproject3;
 public class Mavenproject3 {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 999; i++){
-            if(i % 2 != 0){
-                System.out.println(i);
-            }
-        }
+        System.out.println("Hello word");
     }
 }
