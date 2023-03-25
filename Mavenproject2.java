ff77cc978fa3646d2e6acfa82870daff597e9768
lacctrDress20 <1490244@senati.pe>
2023-03-25 09:23:09 -0500

    creacion de los datos

---
+++ Mavenproject2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        System.out.println("Morning, Morning, Morning");
+        System.out.println("\tHello, have a good day");
+        System.out.println("\t\t Oh, Thank you");
+        System.out.println("\n\t\tYou are great");
+        System.out.println("\tThis is my pleasure");
+        System.out.println("No,... You are welcome");
+
+    }
+}
